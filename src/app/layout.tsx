c2d77237572800b3e15b9b2afdf657fba8849703
Simonas Stonkus <simonas.stonkus@gmail.com>
2024-01-12 22:22:01 +0000

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <MantineProvider>{children}</MantineProvider>
+        <Analytics />
       </body>
     </html>
   );
